60a94eea39999cd9b0dc78dcaf52a846a6c8fdc8
ADiemus <genek14@mail.ru>
2023-06-14 17:24:26 +0300

    промежуточная дз до последнего варианта

---
+++ 2 семинар дз/Exmpl_015/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.(использовать И и ИЛИ)
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Введите день недели, где 1 - Понедельник, а 7 - Воскресенье: ");
+int Date = (Console.ReadLine ()!);
+
+if (Date = 6 || Date = 7 ){
+    Console.WriteLine ("Данный день недели является выходным");
+}
+
+else Console.WriteLine ("Это будний день недели");
