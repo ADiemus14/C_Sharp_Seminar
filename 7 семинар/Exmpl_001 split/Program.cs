f815edbcad7013cd7d01a757c660890b3704040a
ADiemus <genek14@mail.ru>
2023-07-10 17:01:47 +0300

    7seminar

---
+++ 7 семинар/Exmpl_001 split/Program.cs
@@ -0,0 +1,16 @@
+﻿//ввод массива от пользователя 
+ 
+Console.Write("Введите числа через пробел: "); 
+int[] numbers = GetArrayFromString(Console.ReadLine()!); 
+ 
+ 
+int[] GetArrayFromString(string stringArray) 
+{ 
+    string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries); 
+    int[] result = new int[numS.Length]; 
+    for (int i = 0; i < result.Length; i++) 
+    { 
+        result[i] = int.Parse(numS[i]); 
+    } 
+    return result; 
+}
